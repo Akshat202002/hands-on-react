e7b3b9e15da5a31b1158fd25be58404352f92f50
Akshat Samdani <akshatsamdani23@gmail.com>
2022-12-07 20:53:13 +0000

    my ch1ep04 changes

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const pageTitle = document.title;
 
 export default () => {
   // Gives us a variable in memory called count and a function to update it called setCount
   const [count, setCount] = useState(0);
+  useEffect(()=>{
+    // if (count > 0) {
+    //   document.title = `${pageTitle}--${count}`
+    // }
 
+    count && (document.title = `${pageTitle}--${count}`)
+  })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? 'Click to Support' : `Supported ${count} times`}
